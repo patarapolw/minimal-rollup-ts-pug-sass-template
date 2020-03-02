cce02f6bf8839dff3fabc7cf56d759367eede126
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-03-02 21:28:35 +0700

    add data-spa-rendered to prevent double loading

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import './router'
 import './index.scss'
 
-const appEl = document.getElementById('App') as HTMLDivElement
+export const appEl = document.getElementById('App') as HTMLDivElement
 
 appEl.append(document.createElement('app-router'))
